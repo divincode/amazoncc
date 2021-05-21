c1b5ac20b319ae3e271c375f3afaeb0cd88b515b
divincode <devadiga.1@iitj.ac.in>
2021-05-21 15:09:16 +0530

    Created the home page and products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from "./header";
+import Home from "./home";
 
 function App() {
   return (
     <div className="App">
        <Header />
+       <Home/>
     </div>
   );
 }
